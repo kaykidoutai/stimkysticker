b596f6be10908b53488b486a9ee4895b807ed7fc
kay <>
2023-11-08 23:19:32 -0800

    Add support (and test) the QL-570

--- stimkysticker/printers/__init__.py
+++ stimkysticker/printers/__init__.py
@@ -1,10 +1,11 @@
-from .brotherql.ql500 import QL500
+from .brotherql.ql500 import QL500, QL570
 from .brotherql.qldummy import QLDummy
 from .csn_a2_t import CSNA2T
 
 ALL_PRINTERS = (QL500, QLDummy, CSNA2T)
 PRINTER_DICT = {
     QL500.name.casefold(): QL500,
+    QL570.name.casefold(): QL570,
     QLDummy.name.casefold(): QLDummy,
     CSNA2T.name.casefold(): CSNA2T,
 }
--- stimkysticker/printers/brotherql/ql500.py
+++ stimkysticker/printers/brotherql/ql500.py
@@ -8,3 +8,8 @@ from .brotherql import BrotherQl
 class QL500(BrotherQl):
     name = "QL-500"
     SUPPORTED_LABELS: typing.Tuple[Label] = (DK2012, DK2205)
+
+
+class QL570(BrotherQl):
+    name = "QL-570"
+    SUPPORTED_LABELS: typing.Tuple[Label] = (DK2012, DK2205)
